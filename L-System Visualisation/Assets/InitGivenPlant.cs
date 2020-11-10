494ebf879d6d4c11469e270e43a698e5987cdc2b
davvies <davies1044@outlook.com>
2020-11-10 00:59:03 +0000

    L System early prototype, menu almost complete

---
+++ L-System Visualisation/Assets/InitGivenPlant.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using TMPro;
+using System.Text.RegularExpressions;
+public class InitGivenPlant : MonoBehaviour
+{
+    [SerializeField] Canvas plantCanvas = default;  
+
+    [SerializeField] Canvas menuCanvas = default;
+
+    [SerializeField] PlantVisualiser plant;
+    //cacheVisualiser when on click mention not having to read memory each time
+
+    public void OnClickVisualiser() {
+        plantCanvas.gameObject.SetActive(true);
+
+        char axiom = transform.Find("Axiom").GetComponent<TextMeshProUGUI>().text[gameObject.transform.Find("Axiom").GetComponent<TextMeshProUGUI>().text.Length-1];
+        string ruleOne = transform.Find("Rule").gameObject.GetComponent<TextMeshProUGUI>().text;
+    //    string ruleTwo = transform.Find("Rule2").gameObject.GetComponent<TextMeshProUGUI>().text;
+        int maxGenerations = int.Parse((Regex.Replace(transform.Find("MaxGenerations").GetComponent<TextMeshProUGUI>().text,@"[^\d]", "")));
+        float theta = float.Parse(((Regex.Match(transform.Find("Theta").GetComponent<TextMeshProUGUI>().text, @"\d+.+\d").Value)));
+        
+        if(transform.Find("Rule2")){
+            string ruleTwo = transform.Find("Rule2").gameObject.GetComponent<TextMeshProUGUI>().text;
+            plant.parcelableRules.Add(ruleTwo[1],ruleTwo.Substring(4).Replace(")",""));
+        }
+
+        //since the rules are limited to 2, 
+        plant.axiom = axiom;
+        plant.maxIterations = maxGenerations;
+        plant.thetaRotationAngle = theta;
+        plant.parcelableRules.Add(ruleOne[1],ruleOne.Substring(4).Replace(")",""));
+       // if(!ruleTwo.Equals("")) plant.parcelableRules.Add(ruleTwo[1],ruleTwo.Substring(4).Replace(")",""));
+
+        plant.plantName = gameObject.name;
+        menuCanvas.gameObject.SetActive(false);
+    }
+
+}
---
+++ L-System Visualisation/Assets/PlantVisualiser.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class TransformInfo {
+    public Vector3 position; 
+    public Quaternion rotation;
+}
+public class PlantVisualiser : MonoBehaviour
+{
+        ///USE ENUM FOR PARACBLE RULE CHARACTER
+
+    [SerializeField] GameObject Branch; 
+
+    [SerializeField] GameObject GeneratedTree = default;
+
+    [SerializeField] GameObject LeafPrefab;
+    public char axiom { get; set; }
+
+    public string plantName { get; set; }
+    
+    public Dictionary<char,string> parcelableRules = new Dictionary<char, string>();
+
+    public int maxIterations { get; set; }
+
+    public float thetaRotationAngle { get; set; } 
+
+    Stack<TransformInfo> transformInfos;
+
+    string currentString = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ViewDataInput();
+        transform.position = GameObject.Find("Tree").transform.position;
+        transformInfos = new Stack<TransformInfo>();
+       Generate(); 
+    }
+
+    void Generate(){
+        currentString = axiom.ToString();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < maxIterations; i++){
+        foreach(char c in currentString){
+            sb.Append(parcelableRules.ContainsKey(c) ? parcelableRules[c] : c.ToString());
+         }
+         currentString = sb.ToString();
+         sb = new StringBuilder();
+        }
+
+        for(int i = 0; i < currentString.Length; i++){
+            switch(currentString[i]){
+                case 'F':
+                    Vector3 initalPosition = transform.position;
+                    transform.Translate(Vector3.up);
+
+                    GameObject treeSegement = Instantiate(Branch);
+                    treeSegement.transform.SetParent(GeneratedTree.transform);
+                    treeSegement.GetComponent<LineRenderer>().SetPosition(0,initalPosition);
+                    treeSegement.GetComponent<LineRenderer>().SetPosition(1,transform.position);
+                break; 
+                case 'X':
+            
+                break;
+                case '-':
+                transform.Rotate(Vector3.back * thetaRotationAngle);
+                break;
+                case '+':
+                transform.Rotate(Vector3.forward * thetaRotationAngle);
+                break;
+                case '[':
+                transformInfos.Push(new TransformInfo(){position = transform.position, rotation = transform.rotation});
+                break;
+                case ']':
+                TransformInfo ti = transformInfos.Pop();
+                transform.position = ti.position;
+                transform.rotation = ti.rotation;
+                break;
+                case 'Y':
+
+                break; 
+                default:
+                 Debug.LogError("CHARACTER NOT FOUND"+currentString[i].ToString()); 
+                 break;
+            }
+        }
+    }
+    
+    //DEBUG METHOD
+    void ViewDataInput(){
+        string TEMPDELETE = "";
+         foreach(var c in parcelableRules){
+            TEMPDELETE += "Key: "+c.Key+" | "+"Value: "+c.Value;
+         }
+        Debug.Log("| Plant name: "+plantName +" |  "+ axiom+" | "+TEMPDELETE+" | Max Iterations: "+maxIterations+" | Theta: "+thetaRotationAngle+" degrees");
+    }
+
+}
