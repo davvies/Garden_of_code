2e01ef4f965ee7bc4b1976aabf7baeb375f6ff66
davvies <davies1044@outlook.com>
2021-04-17 05:57:32 +0100

    Added menu constant

--- Scripts/MenuLaunch.cs
+++ Scripts/MenuLaunch.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/// <summary>Class <c>MenuLaunch</c> Handles a simple transition to open a scene
-/// </summary>
+/// <summary>Class <c>MenuLaunch</c> Handles a simple transition to open a scene </summary>
 public class MenuLaunch : MonoBehaviour
 {
-    public void OpenFirstScene() => SceneManager.LoadScene(1); //onClick method used in execution of 'begin' button
+    const indexOfFirstScene = 1;
+
+    public void OpenFirstScene() => SceneManager.LoadScene(indexOfFirstScene); //onClick method used in execution of 'begin' button
 }
